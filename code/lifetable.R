8704261c30c51ee5b45336a3dcb868d9dd17d851
netterie <jeanettek@gmail.com>
2016-08-05 15:44:17 -0700

    Initial commit

---
+++ code/lifetable.R
@@ -0,0 +1,31 @@
+
+############################################################################
+## File Name: lifetable.R
+
+## File Purpose: Extract and format country-specific life tables
+## Author: Jeanette Birnbaum
+## Date: 07/07/16
+## Edited on: 
+
+## Additional Comments: 
+#############################################################################
+
+## EDIT BEFORE RUNNING
+############################################################
+# Country and life table selection
+ctry <- c('United States')
+source <- c('ihme')
+
+# Files
+ihme_file <- c('~/Dropbox/BCModel/data/lifetables/lifetable_IHME_GBD_2013_LIFE_TABLE_1990_2013_Y2014M12D17/IHME_GBD_2013_LIFE_TABLE_1990_2013_Y2014M12D17.csv')
+############################################################
+
+############################################################
+# Format and save lifetables for each country
+############################################################
+
+usa <- format_lifetable(ihme_file, 'United States')
+uga <- format_lifetable(ihme_file, 'Uganda')
+
+write.csv(uga, '../data/uga_lifetable.csv')
+
