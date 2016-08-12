92153a022e1ec1912c582d8b524bd54f60777c7c
netterie <jeanettek@gmail.com>
2016-08-12 11:46:37 -0700

    First TZA example

--- code/lifetable.R
+++ code/lifetable.R
@@ -13,12 +13,18 @@
 ## EDIT BEFORE RUNNING
 ############################################################
 # Country and life table selection
-ctry <- c('United States')
-source <- c('ihme')
+#ctry <- c('United States')
+#source <- c('ihme')
 
 # Files
 ihme_file <- c('~/Dropbox/BCModel/data/lifetables/lifetable_IHME_GBD_2013_LIFE_TABLE_1990_2013_Y2014M12D17/IHME_GBD_2013_LIFE_TABLE_1990_2013_Y2014M12D17.csv')
 ############################################################
+
+############################################################
+# Setup
+############################################################
+setwd('~/Documents/jbirnbau/screentreatGlobal')
+source('code/screentreat_library.R')
 
 ############################################################
 # Format and save lifetables for each country
@@ -27,5 +33,10 @@ ihme_file <- c('~/Dropbox/BCModel/data/lifetables/lifetable_IHME_GBD_2013_LIFE_T
 usa <- format_lifetable(ihme_file, 'United States')
 uga <- format_lifetable(ihme_file, 'Uganda')
 
-write.csv(uga, '../data/uga_lifetable.csv')
+write.csv(uga, 'data/uga_lifetable.csv')
+
+tza <- format_lifetable(ihme_file, 'Tanzania')
+
+write.csv(tza, 'data/tza_lifetable.csv')
+
 
