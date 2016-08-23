ecbc84ecd59446e9c25eb4d17bcfa813215a47fa
Jeanette Birnbaum <jbirnbau@rhino3.fhcrc.org>
2016-08-23 09:12:53 -0700

    Fixed results for tza_3c

--- examples/tza_3c/input/user_options.R
+++ examples/tza_3c/input/user_options.R
@@ -17,7 +17,7 @@
 if (!'using_wrapper'%in%ls()) {
     warning('Empyting the workspace')
     rm(list=ls())
-    model_version <- 'tza_3b'
+    model_version <- 'tza_3c'
 
     setwd('~')
     if (grepl('jbirnbau', getwd())) rootdir <- getwd()
@@ -86,7 +86,7 @@ denom <- 100000
 ############################################################
 
 # Stage shift
-HR_advanced = 0.6/0.85
+HR_advanced = 0.35/0.85
 
 # Within stage effects
 instage_screen_benefit_early = 1
